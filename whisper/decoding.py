08e90fb79f02dfb89fd8cb0afd5858d7b6d5b8c3
Kento Yamanaka <32149707+kentoym@users.noreply.github.com>
2023-03-28 00:13:16 -0700

    absolute 0.01

--- whisper/decoding.py
+++ whisper/decoding.py
@@ -489,7 +489,8 @@ class ApplyTimestampRules(LogitFilter):
                 dim=-1
             )
             max_text_token_logprob = logprobs[k, : self.tokenizer.timestamp_begin].max()
-            if timestamp_logprob > min(max_text_token_logprob * 2.0, 0.5):
+#             if timestamp_logprob > max_text_token_logprob:
+            if timestamp_logprob > 0.01:
                 logits[k, : self.tokenizer.timestamp_begin] = -np.inf
 
 
